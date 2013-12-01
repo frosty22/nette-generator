d1f3fbbe52b9e2facbed8e6e197b90b517b54207
frosty22 <ledvinka.vit@gmail.com>
2013-12-01 22:04:53 +0100

    composer init

---
+++ src/NetteGenerator/DI/GeneratorExtension.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace NetteGenerator\DI;
+
+/**
+ *
+ * @copyright Copyright (c) 2013 Ledvinka Vít
+ * @author Ledvinka Vít, frosty22 <ledvinka.vit@gmail.com>
+ *
+ */
+class GeneratorExtension extends \Nette\Config\CompilerExtension
+{
+
+
+	/**
+	 * @var array
+	 */
+	private $defaults = array(
+	);
+
+
+	/**
+	 * Base configuration
+	 */
+	public function loadConfiguration()
+	{
+
+	}
+
+
+}
